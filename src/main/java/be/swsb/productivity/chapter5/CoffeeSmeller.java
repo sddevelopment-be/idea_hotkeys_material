9dcc6c937c63061d12a284cf035388df0e5ba05e
Stijn Dejongh <stijn.c.dejongh@gmail.com>
2025-02-25 05:20:13 +0100

    Add unused imports tot he CoffeeSmeller class

--- src/main/java/be/swsb/productivity/chapter5/CoffeeSmeller.java
+++ src/main/java/be/swsb/productivity/chapter5/CoffeeSmeller.java
@@ -3,11 +3,13 @@ package be.swsb.productivity.chapter5;
 import be.swsb.productivity.chapter5.beans.CoffeeBeans;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.String.format;
+import static java.util.Arrays.asList;
 
 public class CoffeeSmeller {
     public String smell(CoffeeBeans... beans) {
         for (CoffeeBeans bean : beans) {
-            if (!smellsLikeTeenSpirit(bean.scent())) {
+            if (!smellsLikeCoffee(bean.scent())) {
                 return String.format("Ehmm, this bean doesn't smell like coffee at all... What's up with %s?", bean.toString());
             }
         }
