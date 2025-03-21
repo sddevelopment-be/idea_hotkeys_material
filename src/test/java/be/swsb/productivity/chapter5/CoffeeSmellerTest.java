d537bb0dbd5a6a131f7185034d37d4c0bbbb7cad
Stijn Dejongh <stijn@sddevelopment.be>
2025-03-21 08:10:52 +0100

    Add next test case and ensure it run properly

--- src/test/java/be/swsb/productivity/chapter5/CoffeeSmellerTest.java
+++ src/test/java/be/swsb/productivity/chapter5/CoffeeSmellerTest.java
@@ -1,7 +1,6 @@
 package be.swsb.productivity.chapter5;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import be.swsb.productivity.chapter5.beans.CoffeeBeans;
 import be.swsb.productivity.chapter5.beans.MaragoBeans;
 
 import org.assertj.core.api.WithAssertions;
@@ -16,6 +15,20 @@ class CoffeeSmellerTest implements WithAssertions {
 		var result = toTest.smell(new MaragoBeans());
 
 		assertThat(result).isEqualTo("They all smell like coffee");
+	}
+
+	@Test
+	void nonCoffeeSmellsAreDetected() {
+		var toTest = new CoffeeSmeller();
+
+		var result = toTest.smell(new CoffeeBeans() {
+			@Override
+			public String scent() {
+				return "Sweaty";
+			}
+		});
+
+		assertThat(result).contains("Ehmm, this bean doesn't smell like coffee at all...");
 	}
 
 }
